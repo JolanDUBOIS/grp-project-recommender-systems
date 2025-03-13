f586cc830a6a91f7b9f61c0e745fd6dafa1c1eff
Jolan <jolan11.dubois@gmail.com>
2025-03-13 14:24:08 +0100

    feat: created base class for rs

---
+++ src/recommender_systems/base_class_recommender_system.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+import pandas as pd
+
+
+class RecommenderSystem(ABC):
+    """ Abstract class for recommender systems. """
+    
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def fit(self, data: dict[str, pd.DataFrame]):
+        """ Fit the model to the data. """
+        pass
+
+    @abstractmethod
+    def predict(self, user_id: str, time: pd.Timestamp, k: int=10) -> list[str]:
+        """ TODO """
+        pass
+
+    @abstractmethod
+    def evaluate(self):   # TODO: Add parameters to this method
+        """ Evaluate the model on the data. """
+        pass
